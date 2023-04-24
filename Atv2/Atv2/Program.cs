5bcca5ad1545d8a5dadaa60ca1d4fec7ad087aac
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-04-24 19:50:35 -0300

    Add files via upload

--- Atv2/Atv2/Program.cs
+++ Atv2/Atv2/Program.cs
@@ -1,51 +1,42 @@
-﻿using System;
-using System.Linq;
+﻿string[] nomes = new string[8];
 
-class Program
+for (int i = 0; i < 8; i++)
 {
-    static void Main(string[] args)
+    Console.Write($"Digite o nome do {i + 1}º participante: ");
+    nomes[i] = Console.ReadLine();
+}
+
+string[] AmigosSecretos = nomes.ToArray();
+bool Sorteio = false;
+
+while (!Sorteio)
+{
+    Shuffle(AmigosSecretos);
+    Sorteio = true;
+    for (int i = 0; i < nomes.Length; i++)
     {
-        string[] nomes = new string[8];
-
-        for (int i = 0; i < 8; i++)
+        if (AmigosSecretos[i] == nomes[i])
         {
-            Console.Write($"Digite o nome do {i + 1}º participante: ");
-            nomes[i] = Console.ReadLine();
-        }
-
-        string[] AmigosSecretos = nomes.ToArray();
-        bool Sorteio = false;
-
-        while (!Sorteio)
-        {
-            Shuffle(AmigosSecretos);
-            Sorteio = true;
-            for (int i = 0; i < nomes.Length; i++)
-            {
-                if (AmigosSecretos[i] == nomes[i])
-                {
-                    Sorteio = false;
-                    break;
-                }
-            }
-        }
-
-        Console.WriteLine(" ");
-        for (int i = 0; i < nomes.Length; i++)
-        {
-            Console.WriteLine($"{nomes[i]} tirou {AmigosSecretos[i]} como amigo secreto.");
+            Sorteio = false;
+            break;
         }
     }
+}
 
-    static void Shuffle<T>(T[] array)
+Console.WriteLine(" ");
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"{nomes[i]} tirou {AmigosSecretos[i]} como amigo secreto.");
+}
+
+static void Shuffle<T>(T[] array)
+{
+    Random random = new Random();
+    for (int i = array.Length - 1; i >= 1; i--)
     {
-        Random random = new Random();
-        for (int i = array.Length - 1; i >= 1; i--)
-        {
-            int j = random.Next(i + 1);
-            T temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
-        }
+        int j = random.Next(i + 1);
+        T temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
     }
 }
