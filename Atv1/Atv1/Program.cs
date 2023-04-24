40ca13aeace51094e5136009937a5a5f023fabf2
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-04-24 19:04:10 -0300

    Add files via upload

---
+++ Atv1/Atv1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Ler a hora de início do jogo
+        Console.Write("Digite a hora de início do jogo (hora:minuto): ");
+        string[] inicio = Console.ReadLine().Split(':');
+        int HoraInicio = int.Parse(inicio[0]);
+        int MinutoInicio = int.Parse(inicio[1]);
+        // Ler a hora de término do jogo
+        Console.Write("Digite a hora de término do jogo (hora:minuto): ");
+        string[] fim = Console.ReadLine().Split(':');
+        int HoraFim = int.Parse(fim[0]);
+        int MinutoFim = int.Parse(fim[1]);
+        // Calcular a duração do jogo em minutos
+        int DuracaoMinutos = (HoraFim * 60 + MinutoFim) - (HoraInicio * 60 + MinutoInicio);
+        // Tratar caso o jogo tenha ultrapassado 24 horas
+        if (DuracaoMinutos < 0)
+        {
+            DuracaoMinutos += 1440;
+        }
+        // Calcular a duração do jogo em horas e minutos
+        int DuracaoHoras = DuracaoMinutos / 60;
+        DuracaoMinutos = DuracaoMinutos % 60;
+        // Escrever a duração do jogo
+        Console.WriteLine("A duração do jogo foi de " + DuracaoHoras + " horas e " + DuracaoMinutos + " minutos.");
+    }
+}
---
+++ Atv4/Atv4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Ler a altura da pessoa
+        Console.Write("Digite a altura da pessoa (em metros): ");
+        double altura = double.Parse(Console.ReadLine());
+
+        // Ler o sexo da pessoa
+        Console.Write("Digite o sexo da pessoa (M para masculino e F para feminino): ");
+        char sexo = char.Parse(Console.ReadLine());
+
+        // Calcular o peso ideal da pessoa de acordo com o sexo
+        double pesoIdeal;
+        if (char.ToLower(sexo) == 'M')
+        {
+            pesoIdeal = (72.7 + altura) - 58;
+        }
+        else if (char.ToLower(sexo) == 'F')
+        {
+            pesoIdeal = (62.1) - 44.7;
+        }
+        else
+        {
+            Console.WriteLine("Sexo inválido.");
+            return;
+        }
+        // Escrever o peso ideal da pessoa
+        Console.WriteLine("O peso ideal da pessoa é " + pesoIdeal + " kg.");
+    }
+}
